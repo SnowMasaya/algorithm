9f715141a83c910b294c45aa33660ae4d9f5b178
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-04-07 07:46:14 +0900

    update

--- algorithm_study/replaceword.cpp
+++ algorithm_study/replaceword.cpp
@@ -23,6 +23,7 @@ void replace_word::replace_word_method(string original_word, string compare_word
 	bool same_flag = false;
 	bool upper_flag = false;
 	bool lower_flag = false;
+	unordered_map<char, int> original_word_hash;
 
 	if (original_word.length() == compare_word.length()){
 		same_flag = true;
@@ -33,15 +34,45 @@ void replace_word::replace_word_method(string original_word, string compare_word
 	}
 
 	if (original_word.length() + 2 < compare_word.length()){
-	    cerr << "Compare original and compare " << original_word << " " << compare_word << endl;
+	    cerr << "*NG* Compare original and compare " << original_word << " " << compare_word << endl;
         return ;
 	}
 
 	if (original_word.length() -2 > compare_word.length()){
-	    cerr << "Compare original and compare " << original_word << " " << compare_word << endl;
+	    cerr << "*NG* Compare original and compare " << original_word << " " << compare_word << endl;
         return ;
 	}
 
+	// Make Original Word Hash
+	for(auto it = original_word.begin(); it != original_word.end(); ++it){
+		if (original_word_hash.find(*it) == original_word_hash.end()){
+			original_word_hash.insert({*it, 1});
+		}
+	}
+
+	int not_same_word_number = 0;
+	for(auto it = compare_word.begin(); it != compare_word.end(); it++){
+		auto same_char = original_word_hash.find(*it);
+		if (same_char == original_word_hash.end()){
+            not_same_word_number++;
+		}
+	}
+
+	if(not_same_word_number > 2){
+	    cerr << "*NG* Compare original and compare " << original_word << " " << compare_word << endl;
+        return ;
+	}
+
+	if (original_word.length() == compare_word.length()){
+		if(not_same_word_number > 1){
+	        cerr << "*NG* Compare original and compare " << original_word << " " << compare_word << endl;
+            return ;
+		}else{
+	        cout << "*OK* Compare original and compare " << original_word << " " << compare_word << endl;
+            return ;
+		}
+
+	}
 
 }
 } /* namespace replace_word */
